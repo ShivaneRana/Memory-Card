a02cb15533f2437cd232d0dbff60b439474e1915
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-13 23:36:27 +0530

    new component added

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import {useContext } from "react";
+import { mainContext } from "./App";
+import style from "./styles/Navbar.module.css";
+
+function Navbar(){
+    const context = useContext(mainContext);
+
+    return(
+        <h1></h1>
+    )
+}
+
+export default Navbar;
