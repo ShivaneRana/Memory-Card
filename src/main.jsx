2b6d995548bf406c0eb870323932de0a7227afba
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-13 15:27:16 +0530

    lenis added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Lenis from "lenis";
 import App from "./App.jsx";
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
