8439f29985e02665a9c3135d59485a5b05ca5929
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-13 23:35:52 +0530

    mainContainer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useState,createContext } from "react";
+import { useImmer } from "use-immer";
 import style from "./App.module.css";
+import Navbar from "./Navbar.jsx";
+
+
+export const mainContext = createContext();
 
 function App() {
-  return <></>;
+  return <MainContainer></MainContainer> 
+
+}
+
+function MainContainer(){
+  return(
+  <mainContext.Provider value={{}}>
+    <div className={style.mainContainer}>
+      <Navbar></Navbar>
+    </div>
+  </mainContext.Provider>
+  )
 }
 
 export default App;
