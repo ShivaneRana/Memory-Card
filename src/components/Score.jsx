705019ba55f84fb7c98d8afefe6079d079fdaba0
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 18:49:03 +0530

    current score and best score header added

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -5,7 +5,10 @@ import { useContext } from "react";
 function Score() {
   const context = useContext(mainContext);
 
-  return <></>;
+  return <div className={style.mainContainer}>
+    <h3 title="Current score">Score: <span>{context.currentScore}</span></h3>
+    <h3 title="Best score">Best: <span>{context.topScore}</span></h3>
+  </div>;
 }
 
 export default Score;
