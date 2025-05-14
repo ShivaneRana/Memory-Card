159e5484b518011279a5a3b87a269f5d9ffd831b
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 17:14:21 +0530

    new component created and base code added

---
+++ src/components/Score.jsx
@@ -0,0 +1,14 @@
+import style from "../styles/Score.module.css";
+import { mainContext } from "../App.jsx";
+import { useContext } from "react";
+
+function Score(){
+    const context = useContext(mainContext);
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Score;
