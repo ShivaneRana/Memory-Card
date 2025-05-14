d2a61403b0209bc11fd38dd13a7594b99be92305
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 22:16:20 +0530

    context imported and initialized using useContext

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,11 @@
 import style from "../styles/Content.module.css";
 import { mainContext } from "../App.jsx";
+import { useContext } from "react";
 
 function Content() {
-  return <div className={style.mainContainer}></div>;
+  const context = useContext(mainContext);
+  return <div className={style.mainContainer}>
+  </div>;
 }
 
 export default Content;
