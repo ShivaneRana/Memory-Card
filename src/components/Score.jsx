ec5a213359fe2b1f96d911c9928981eb5876710f
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 18:49:27 +0530

    prettier used

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -5,10 +5,16 @@ import { useContext } from "react";
 function Score() {
   const context = useContext(mainContext);
 
-  return <div className={style.mainContainer}>
-    <h3 title="Current score">Score: <span>{context.currentScore}</span></h3>
-    <h3 title="Best score">Best: <span>{context.topScore}</span></h3>
-  </div>;
+  return (
+    <div className={style.mainContainer}>
+      <h3 title="Current score">
+        Score: <span>{context.currentScore}</span>
+      </h3>
+      <h3 title="Best score">
+        Best: <span>{context.topScore}</span>
+      </h3>
+    </div>
+  );
 }
 
 export default Score;
