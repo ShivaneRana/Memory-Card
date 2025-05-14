0321739be6d6dfb4057ca59cf45ef9684b6ed6c2
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 21:37:23 +0530

    new component created and base code added

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import style from "../styles/Content.module.css";
+import {mainContext} from "../App.jsx";
+
+function Content(){
+    return(
+        <div className={style.mainContainer}>
+        </div>
+    )
+}
+
+export default Content;
