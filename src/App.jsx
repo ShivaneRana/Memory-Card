b951515573c02f9ae73ba540a8b493df722f0d60
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 17:08:09 +0530

    added sound,currentScore and topScore state and added to context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, createContext } from "react";
 import { useImmer } from "use-immer";
 import style from "./App.module.css";
-import Navbar from "./Navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 export const mainContext = createContext();
 
@@ -10,8 +10,31 @@ function App() {
 }
 
 function MainContainer() {
+  const [sound,setSound] = useState(true);
+  const [currentScore,setCurrentScore] = useState(0);
+  const [topScore,setTopScore] = useState(0);
+
+  function toggleSound(){
+    setSound(!sound);
+  }
+
+  function updateCurrentScore(){
+    setCurrentScore(currentScore + 1);
+  }
+
+  function updateTopScore(){
+    setTopScore(currentScore);
+  }
+
   return (
-    <mainContext.Provider value={{}}>
+    <mainContext.Provider value={{
+      sound,
+      currentScore,
+      topScore,
+      toggleSound,
+      updateCurrentScore,
+      updateTopScore
+    }}>
       <div className={style.mainContainer}>
         <Navbar></Navbar>
       </div>
