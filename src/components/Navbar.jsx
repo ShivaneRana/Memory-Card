382c105aebfddf71c64f7fa4600ba01bab68845b
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 16:51:28 +0530

    prettier used

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,12 @@ import infoIcon from "./assets/images/info.svg";
 function Navbar() {
   const context = useContext(mainContext);
 
-  return(
+  return (
     <div className={style.mainContainer}>
-        <h1>Memory Card</h1>
-        <div className={style.buttonHolder}>
+      <h1>Memory Card</h1>
+      <div className={style.buttonHolder}>
         <button title="Toggle sound effect">
-          <img src={muteIcon}  alt="toggling sound effect icon"></img>
+          <img src={muteIcon} alt="toggling sound effect icon"></img>
         </button>
         <button title="Reset game">
           <img src={resetIcon} alt="reset game icon"></img>
@@ -22,9 +22,9 @@ function Navbar() {
         <button title="Game information">
           <img src={infoIcon} alt="information icon"></img>
         </button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
