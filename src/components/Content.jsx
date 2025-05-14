2d147473d50d4a0308bc1ec7bec3dab5bb07e76a
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-14 21:46:12 +0530

    prettier used

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,8 @@
 import style from "../styles/Content.module.css";
-import {mainContext} from "../App.jsx";
+import { mainContext } from "../App.jsx";
 
-function Content(){
-    return(
-        <div className={style.mainContainer}>
-        </div>
-    )
+function Content() {
+  return <div className={style.mainContainer}></div>;
 }
 
 export default Content;
