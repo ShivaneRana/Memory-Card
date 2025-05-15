df8477a6f6efc817e0674c742a812e381767f33f
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-15 18:04:54 +0530

    work in progress

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,26 @@ import { useContext } from "react";
 
 function Content() {
     const context = useContext(mainContext);
-    return <div className={style.mainContainer}></div>;
+    return (
+    <div className={style.mainContainer}>
+        <ImageHolder></ImageHolder>   
+    </div>
+    )
+}
+
+function ImageHolder(){
+    return(
+        <div className={style.content}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
 }
 
 export default Content;
