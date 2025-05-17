0377721056c2d9e3060c7cc7d20d497c60220e68
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-17 15:43:46 +0530

    prettier used

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 import style from "../styles/Content.module.css";
 import { mainContext } from "../App.jsx";
-import {useImmer} from "use-immer";
-import { createContext,useContext, useEffect, useState } from "react";
+import { useImmer } from "use-immer";
+import { createContext, useContext, useEffect, useState } from "react";
 import loadingIcon from "../assets/images/loading.svg";
 import { useSyncExternalStore } from "react";
 
@@ -28,29 +28,35 @@ function ImageHolder() {
     );
 }
 
-function Images(){
-    const [selected,setSelected] = useState(0);
-    const randomNumber = Math.floor(Math.random()*500);
-    const data = useData(randomNumber)
+function Images() {
+    const [selected, setSelected] = useState(0);
+    const randomNumber = Math.floor(Math.random() * 500);
+    const data = useData(randomNumber);
 
-    return(
+    return (
         <button title={data && data.name}>
-            {data === null ? <p>Loading...</p> : <img src={data.sprites.front_default}></img>}
+            {data === null ? (
+                <p>Loading...</p>
+            ) : (
+                <img src={data.sprites.front_default}></img>
+            )}
         </button>
-    )
+    );
 }
 
 function useData(id) {
-    const [data,setData] = useState(null);
+    const [data, setData] = useState(null);
 
     useEffect(() => {
         let ignore = false;
-        if(!ignore){
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{mode:"cors"}).then(result => result.json()).then(finalResult => setData(finalResult));
+        if (!ignore) {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, { mode: "cors" })
+                .then((result) => result.json())
+                .then((finalResult) => setData(finalResult));
         }
 
-        return () => ignore = true;
-    },[])
+        return () => (ignore = true);
+    }, []);
 
     return data;
 }
